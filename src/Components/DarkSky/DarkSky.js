65ab17f62ff622a337b3f1aecbcbfd62296047ab
Aleksandr Gulbickij <4efirr@gmail.com>
2019-04-25 22:14:18 +0300

    Weather API is called on demand ( button click )

--- src/Components/DarkSky/DarkSky.js
+++ src/Components/DarkSky/DarkSky.js
@@ -4,6 +4,7 @@ class CallDarkSky extends React.Component {
   constructor(props) {
     super(props);
     this.state = { apiResponse: "", moonPhases: [] };
+    this.getForecast = this.getForecast.bind(this);
   }
 
   responseFormat( response ){
@@ -26,29 +27,46 @@ class CallDarkSky extends React.Component {
     });
   }
 
+  getForecast(e){
+    e.preventDefault();
+    fetch("http://localhost:9000/testAPI")
+          .then(res => res.text())
+          .then(res => this.responseFormat(res) );
+  }
+
   callAPI() {
     // fetch("http://localhost:9000/testAPI")
     //       .then(res => res.text())
     //       .then(res => this.responseFormat(res) );
   }
+
   componentWillMount() {
     this.callAPI();
   }
 
   render() {
-    const phaseList = this.state.moonPhases.map((item, index) => (
-      <li key={item} className="list-group-item"> {item} </li>
-    ));
+    let weatherData = "";
+    if (this.state.currentTemp){
+      const phaseList = this.state.moonPhases.map((item, index) => (
+        <li key={item} className="list-group-item"> {item} </li>
+      ));
 
+      weatherData = <div className="col mt-4 mb-4">
+        <p>Current temperature is {this.state.currentTemp} </p>
+        <ul className="list-group list-group-horizontal">
+          {phaseList}
+        </ul>
+      </div>
+    }
 
     return (
       <div className="row">
         <div className="col">
-          <p>Current temperature is {this.state.currentTemp} </p>
-          <ul className="list-group list-group-horizontal">
-            {phaseList}
-          </ul>
+          <button
+            onClick={(e)=>this.getForecast(e)}
+            className="btn btn-primary btn-lg">Get forecast</button>
         </div>
+        {weatherData}
       </div>
     );
   }
